d5aa6c5716c2054550c02f36bae93975992a5083
funnycat53 <patriks.jkb@gmail.com>
2025-01-12 14:50:23 +0200

    Update klases.py

--- klases.py
+++ klases.py
@@ -38,32 +38,54 @@ class List:
         self.skaits += 1
         return
 
-    def read(self):
-        if self.sakums == None:
-            print("Saraksts ir tukšs!")
-        esosais = self.sakums
-        while esosais:
-            esosais.read()
-            esosais = esosais.next
-        return
+    def read(self, index=-1):
+            if self.sakums is None:
+                print("Saraksts ir tukšs!")
+                return
+            if index == -1:
+                esosais = self.sakums
+                while esosais:
+                    esosais.read()
+                    esosais = esosais.next
+            else:
+                if index < 0 or index >= self.skaits:
+                    print("Norādītais indekss neatrodas sarakstā!")
+                    return
+                esosais = self.sakums
+                for i in range(index):
+                    esosais = esosais.next
+                esosais.read()
+            return
 
-    def pop(self):
+    def pop(self, index = -1):
         if self.skaits == 0:
             print("Nav ko izdzēst")
             return
-        if self.skaits == 1:
-            self.sakums = None
-            self.skaits = 0
-            return
-        pirmspedejais = self.sakums
-        while pirmspedejais.next.next:
-            pirmspedejais = pirmspedejais.next
-        pirmspedejais.next = None
+        if index == -1 or index >= self.skaits - 1:
+            if self.skaits == 1:
+                self.sakums = None
+            else:
+                pirmspedejais = self.sakums
+                while pirmspedejais.next.next:
+                    pirmspedejais = pirmspedejais.next
+                pirmspedejais.next = None
+        elif index == 0:
+            self.sakums = self.sakums.next
+            if self.sakums:
+                self.sakums.prev = None
+        else:
+            esosais = self.sakums
+            for i in range(index):
+                esosais = esosais.next
+            galva = esosais.prev
+            aste = esosais.next
+            galva.next = aste
+            if aste:
+                aste.prev = galva
         self.skaits -= 1
         return
-    
+        
 saraksts = List()
-saraksts.read()
 saraksts.add("free", 1)
 saraksts.add("huzz", 2)
 saraksts.add("luh", 0)
@@ -72,6 +94,6 @@ saraksts.add("tēma", 3)
 saraksts.add("bruzz", 6)
 saraksts.add("tuzz", 5)
 saraksts.add(1000000)
-saraksts.pop()
-saraksts.read()
+saraksts.pop(5)
+saraksts.read(29)
 
