b4f00888de75d90eb18641d53036b1876f065778
funnycat53 <patriks.jkb@gmail.com>
2025-02-05 13:13:03 +0200

    omg

--- maju_meklesana/algoritms.py
+++ maju_meklesana/algoritms.py
@@ -5,34 +5,55 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 
-bildes = []
-label = []
-
-bilzu_adrese = "maju_meklesana/bildes/"
-
-for nosaukums in os.listdir(bilzu_adrese):
-    image = Image.open(os.path.join(bilzu_adrese,nosaukums)).resize((200,200), Image.Resampling.NEAREST)
-    bildes.append(np.array(image))
-    tests = np.array(bildes)
-    if "maja" in nosaukums:
-        label.append(1)
-    else:
-        label.append(0)
-
-bildes = np.array(bildes)
-label = np.array(label)
-
-bildes = bildes/255.0 
-bildes = bildes.reshape(bildes.shape[0], -1)
-
-X_train, X_test, y_train, y_test = train_test_split(bildes, label, test_size=3)
 
 modelis = RandomForestClassifier()
+bilzu_adrese = "maju_meklesana/bildes/"
 
-modelis.fit(X_train, y_train)
+def modela_trenesana(modelis):
+    bildes = []
+    label = []
 
-paregojums = modelis.predict(X_test)
+    for nosaukums in os.listdir(bilzu_adrese):
+        image = Image.open(os.path.join(bilzu_adrese,nosaukums)).resize((200,200), Image.Resampling.NEAREST)
+        bildes.append(np.array(image))
+        if "maja" in nosaukums:
+            label.append(1)
+        else:
+            label.append(0)
 
-precizitate = accuracy_score(y_test, paregojums)
+    bildes = np.array(bildes)
+    label = np.array(label)
 
-print(precizitate)
+    bildes = bildes/255.0 
+    bildes = bildes.reshape(bildes.shape[0], -1)
+
+    X_train, X_test, y_train, y_test = train_test_split(bildes, label, test_size=3)
+
+    modelis.fit(X_train, y_train)
+
+    paregojums = modelis.predict(X_test)
+
+    precizitate = accuracy_score(y_test, paregojums)
+
+    print(precizitate)
+    return(modelis)
+
+def majas_noteiksana(bilde, modelis):
+    bilde = [Image.open(bilde).resize((200,200), Image.Resampling.NEAREST)]
+    bilde = np.array(bilde)
+    bilde = bilde/255.0
+    bilde = bilde.reshape(bilde.shape[0], -1)
+    rezultats = modelis.predict(bilde)
+    if rezultats[0] == 1:
+        print("Šajā bildē ir māja")
+    else:
+        print("Šajā bildē nav māja")
+    print(rezultats)
+    return rezultats
+
+modelis = modela_trenesana(modelis)
+
+majas_noteiksana("maju_meklesana/tests.jpg", modelis)
+
+
+    
